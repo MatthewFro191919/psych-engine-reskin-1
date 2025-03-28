0133abe1b3d06464a9086ed33ca617e11af1eb66
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-03-28 15:39:40 -0400

    Update Paths.hx

--- source/backend/Paths.hx
+++ source/backend/Paths.hx
@@ -31,7 +31,7 @@ class Paths
 
 	inline static public function script(key:String, ?library:String):String
 	{
-		return getPath('assets/$folder/$file.hx', TEXT, library);
+		return getPath('$key.hx', TEXT, library);
 	}
 
 	public static function excludeAsset(key:String) {
